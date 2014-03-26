a176c10f0f7f867511314412d9cb88c2299b2517
Weyman Fung <wfung0083@yahoo.com>
2014-03-26 16:00:10 -0700

    Fixing things up

--- app/assets/javascripts/views/polls/allpoll_index.js
+++ app/assets/javascripts/views/polls/allpoll_index.js
@@ -2,7 +2,6 @@ window.RolyPolly.Views.AllPollsIndex = Backbone.View.extend({
 	template: JST["poll/allpoll_index"],
 
 	initialize: function(options) {
-		this.listenTo(this.collection, "sync add remove", this.render)
 	},
 
 	render: function() {
